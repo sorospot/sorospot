647e574534105e06fcadefea20a9430472266cdf
Guilherme <guisxares@hotmail.com>
2025-10-26 18:07:33 -0300

    Adiciona exibição de usuário logado e botão de logout na home

--- sorospot/src/main/java/br/com/sorospot/controllers/HomeController.java
+++ sorospot/src/main/java/br/com/sorospot/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package br.com.sorospot.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, HttpSession session) {
         model.addAttribute("pageTitle", "Página Inicial");
         model.addAttribute("contentTemplate", "exemplos/home");
+        
+        String userName = (String) session.getAttribute("userName");
+        if (userName != null) {
+            model.addAttribute("userName", userName);
+            model.addAttribute("isLoggedIn", true);
+        } else {
+            model.addAttribute("isLoggedIn", false);
+        }
+        
         return "exemplos/layout";
     }
 }
