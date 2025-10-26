ddfb910627dabe114d77049c75ebfc5e187b9007
Guilherme <guisxares@hotmail.com>
2025-10-26 19:42:38 -0300

    Criado o UserRoleEnum para ajudar no gerenciamento de permissões no projeto

--- sorospot/src/main/java/br/com/sorospot/config/DataInitializer.java
+++ sorospot/src/main/java/br/com/sorospot/config/DataInitializer.java
@@ -1,14 +1,13 @@
 package br.com.sorospot.config;
 
 import br.com.sorospot.domain.UserRole;
+import br.com.sorospot.domain.UserRoleEnum;
 import br.com.sorospot.repositories.user.UserRoleRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class DataInitializer {
@@ -22,9 +21,8 @@ public class DataInitializer {
     }
 
     private void initializeUserRoles() {
-        List<String> defaultRoles = Arrays.asList("USER", "ADMIN", "PUBLIC_ENTITIES");
-
-        for (String roleName : defaultRoles) {
+        for (UserRoleEnum roleEnum : UserRoleEnum.values()) {
+            String roleName = roleEnum.getRole();
             if (!userRoleRepository.findByUserRole(roleName).isPresent()) {
                 UserRole role = new UserRole();
                 role.setUserRole(roleName);
