e21d46bad8e032862947ab91ae4c580def538a8a
Guilherme <guilherme.goncalves01@hotmail.com>
2025-10-26 20:56:12 -0300

    corrigido HomeController

--- sorospot/src/main/java/br/com/sorospot/controllers/HomeController.java
+++ sorospot/src/main/java/br/com/sorospot/controllers/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String mapa(Model model) {
+    @GetMapping("/home")
+    public String home(Model model) {
         model.addAttribute("pageTitle", "Home");
         model.addAttribute("contentTemplate", "exemplos/home.html");
         return "exemplos/layout.html";
