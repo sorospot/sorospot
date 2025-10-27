e3b08c67bf4b1fcf55e9ea4d7a046ff6b9f5d1fa
Guilherme <guisxares@hotmail.com>
2025-10-26 23:11:10 -0300

    Excluido userRoleRepository a mais

--- sorospot/src/main/java/br/com/sorospot/repositories/UserRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.sorospot.repositories;
-
-import br.com.sorospot.domains.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
-}
