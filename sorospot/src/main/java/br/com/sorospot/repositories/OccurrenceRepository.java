c3a193d12c5d3ff72061ba82b179774c9932f207
Ágatha Corrêa <agathacduarte@gmail.com>
2025-10-27 00:09:45 -0300

    Add @Repository annotation to OccurrenceRepository

--- sorospot/src/main/java/br/com/sorospot/repositories/OccurrenceRepository.java
+++ sorospot/src/main/java/br/com/sorospot/repositories/OccurrenceRepository.java
@@ -2,6 +2,8 @@ package br.com.sorospot.repositories;
 
 import br.com.sorospot.domains.Occurrence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OccurrenceRepository extends JpaRepository<Occurrence, Integer> {
 }
