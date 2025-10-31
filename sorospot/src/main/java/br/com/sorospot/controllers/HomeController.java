c8cf71430a749ab4511477f7d0331499a750c8bf
guisxares <guisxares@hotmail.com>
2025-10-30 21:50:34 -0300

    Adicionado homePage (enquanto ainda existir) na verificação de usuários também.

--- sorospot/src/main/java/br/com/sorospot/controllers/HomeController.java
+++ sorospot/src/main/java/br/com/sorospot/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package br.com.sorospot.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // @GetMapping("/home")
-    // public String home(Model model) {
-    //     model.addAttribute("pageTitle", "Home");
-    //     model.addAttribute("contentTemplate", "exemplos/home");
-    //     return "exemplos/layout";
-    // }
+    @GetMapping("/home")
+    public String home(HttpSession session, Model model) {
+        if (session.getAttribute("loggedUser") == null) {
+            return "redirect:/signIn";
+        }
+
+        model.addAttribute("pageTitle", "Home");
+        model.addAttribute("contentTemplate", "exemplos/home");
+        return "exemplos/layout";
+    }
 }
