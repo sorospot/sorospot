16a246003456a06358fa5d74c5f85ae25f289a1f
Ágatha Corrêa <agathacduarte@gmail.com>
2025-10-31 22:51:49 -0300

    Criando css da página de detalhes da ocorrência

--- sorospot/src/main/java/br/com/sorospot/config/security/SecurityConfig.java
+++ sorospot/src/main/java/br/com/sorospot/config/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/signIn", "/signUp", "/home", "/mapa", "/api/**", "/css/**", "/js/**", "/images/**", "/static/**", "/uploads/**").permitAll()
+                .requestMatchers("/", "/signIn", "/signUp", "/home", "/mapa", "/api/**", "/css/**", "/js/**", "/images/**", "/static/**", "/uploads/**", "/occurrence/show/**").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
