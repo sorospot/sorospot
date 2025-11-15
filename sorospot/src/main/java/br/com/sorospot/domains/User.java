e05e1b9503b25285f73a747d3a3eb67bbfe55f68
Ágatha Corrêa <agathacduarte@gmail.com>
2025-11-15 00:46:38 -0300

    Adicionando suporte para exibir informações do usuário na visualização da ocorrência

--- sorospot/src/main/java/br/com/sorospot/domains/User.java
+++ sorospot/src/main/java/br/com/sorospot/domains/User.java
@@ -65,6 +65,14 @@ public class User {
         this.lastUpdated = lastUpdated;
     }
     
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
